63fb81f064a8a0e6a8ddd8f870074eda3f0197b6
David Ahern <dsa@cumulusnetworks.com>
2016-10-17 19:15:51 -0700

    net: Remove all_adj_list and its references
    Only direct adjacencies are maintained. All upper or lower devices can
    be learned via the new walk API which recursively walks the adj_list for
    upper devices or lower devices.
    
    Signed-off-by: David Ahern <dsa@cumulusnetworks.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit f1170fd462c67c4ae2f20734566d94e0f8f62f69)

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1422,7 +1422,6 @@ enum netdev_priv_flags {
  *	@close_list:	List entry, that is used, when we are closing the device
  *
  *	@adj_list:	Directly linked devices, like slaves for bonding
- *	@all_adj_list:	All linked devices, *including* neighbours
  *	@features:	Currently active device features
  *	@hw_features:	User-changeable features
  *
@@ -1642,11 +1641,6 @@ struct net_device {
 		struct list_head upper;
 		struct list_head lower;
 	} adj_list;
-
-	struct {
-		struct list_head upper;
-		struct list_head lower;
-	} all_adj_list;
 
 	netdev_features_t	features;
 	netdev_features_t	hw_features;
@@ -3815,13 +3809,6 @@ struct net_device *netdev_all_upper_get_next_dev_rcu(struct net_device *dev,
 	     updev; \
 	     updev = netdev_upper_get_next_dev_rcu(dev, &(iter)))
 
-/* iterate through upper list, must be called under RCU read lock */
-#define netdev_for_each_all_upper_dev_rcu(dev, updev, iter) \
-	for (iter = &(dev)->all_adj_list.upper, \
-	     updev = netdev_all_upper_get_next_dev_rcu(dev, &(iter)); \
-	     updev; \
-	     updev = netdev_all_upper_get_next_dev_rcu(dev, &(iter)))
-
 int netdev_walk_all_upper_dev_rcu(struct net_device *dev,
 				  int (*fn)(struct net_device *upper_dev,
 					    void *data),
@@ -3860,18 +3847,6 @@ struct net_device *netdev_all_lower_get_next(struct net_device *dev,
 					     struct list_head **iter);
 struct net_device *netdev_all_lower_get_next_rcu(struct net_device *dev,
 						 struct list_head **iter);
-
-#define netdev_for_each_all_lower_dev(dev, ldev, iter) \
-	for (iter = (dev)->all_adj_list.lower.next, \
-	     ldev = netdev_all_lower_get_next(dev, &(iter)); \
-	     ldev; \
-	     ldev = netdev_all_lower_get_next(dev, &(iter)))
-
-#define netdev_for_each_all_lower_dev_rcu(dev, ldev, iter) \
-	for (iter = (dev)->all_adj_list.lower.next, \
-	     ldev = netdev_all_lower_get_next_rcu(dev, &(iter)); \
-	     ldev; \
-	     ldev = netdev_all_lower_get_next_rcu(dev, &(iter)))
 
 int netdev_walk_all_lower_dev(struct net_device *dev,
 			      int (*fn)(struct net_device *lower_dev,
