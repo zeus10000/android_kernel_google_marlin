10d4ce5fa626455e900dd760630fddb4028a97fb
Gianluca Borello <g.borello@gmail.com>
2016-12-03 12:31:33 -0800

    bpf: Preserve const register type on const OR alu ops
    Occasionally, clang (e.g. version 3.8.1) translates a sum between two
    constant operands using a BPF_OR instead of a BPF_ADD. The verifier is
    currently not handling this scenario, and the destination register type
    becomes UNKNOWN_VALUE even if it's still storing a constant. As a result,
    the destination register cannot be used as argument to a helper function
    expecting a ARG_CONST_STACK_*, limiting some use cases.
    
    Modify the verifier to handle this case, and add a few tests to make sure
    all combinations are supported, and stack boundaries are still verified
    even with BPF_OR.
    
    Change-Id: I5addb9e1549acce9b84722a71d95ae61c60bb2ad
    Signed-off-by: Gianluca Borello <g.borello@gmail.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 3c839744b33782b930c5c61df35511ede5e5a574)

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -1639,6 +1639,11 @@ static int evaluate_reg_imm_alu(struct bpf_verifier_env *env,
 	else if (opcode == BPF_ADD && BPF_SRC(insn->code) == BPF_X &&
 		 src_reg->type == CONST_IMM)
 		dst_reg->imm += src_reg->imm;
+	else if (opcode == BPF_OR && BPF_SRC(insn->code) == BPF_K)
+		dst_reg->imm |= insn->imm;
+	else if (opcode == BPF_OR && BPF_SRC(insn->code) == BPF_X &&
+		 src_reg->type == CONST_IMM)
+		dst_reg->imm |= src_reg->imm;
 	else
 		mark_reg_unknown_value(regs, insn->dst_reg);
 	return 0;
