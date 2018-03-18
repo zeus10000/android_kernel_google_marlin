0b557cd22bef9e0fc656b6d049dc7269d13ddc20
John Fastabend <john.fastabend@gmail.com>
2018-03-18 12:57:15 -0700

    bpf: sockmap, add bpf_msg_apply_bytes() helper
    A single sendmsg or sendfile system call can contain multiple logical
    messages that a BPF program may want to read and apply a verdict. But,
    without an apply_bytes helper any verdict on the data applies to all
    bytes in the sendmsg/sendfile. Alternatively, a BPF program may only
    care to read the first N bytes of a msg. If the payload is large say
    MB or even GB setting up and calling the BPF program repeatedly for
    all bytes, even though the verdict is already known, creates
    unnecessary overhead.
    
    To allow BPF programs to control how many bytes a given verdict
    applies to we implement a bpf_msg_apply_bytes() helper. When called
    from within a BPF program this sets a counter, internal to the
    BPF infrastructure, that applies the last verdict to the next N
    bytes. If the N is smaller than the current data being processed
    from a sendmsg/sendfile call, the first N bytes will be sent and
    the BPF program will be re-run with start_data pointing to the N+1
    byte. If N is larger than the current data being processed the
    BPF verdict will be applied to multiple sendmsg/sendfile calls
    until N bytes are consumed.
    
    Note1 if a socket closes with apply_bytes counter non-zero this
    is not a problem because data is not being buffered for N bytes
    and is sent as its received.
    
    Change-Id: I2df20bc3afe41f1d1ebb17947130c478499f9f8a
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>
    Acked-by: David S. Miller <davem@davemloft.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    (cherry picked from commit 2a100317c9ebc204a166f16294884fbf9da074ce)

--- net/core/filter.c
+++ net/core/filter.c
@@ -1947,6 +1947,20 @@ static const struct bpf_func_proto bpf_msg_redirect_map_proto = {
 	.arg4_type      = ARG_ANYTHING,
 };
 
+BPF_CALL_2(bpf_msg_apply_bytes, struct sk_msg_buff *, msg, u32, bytes)
+{
+	msg->apply_bytes = bytes;
+	return 0;
+}
+
+static const struct bpf_func_proto bpf_msg_apply_bytes_proto = {
+	.func           = bpf_msg_apply_bytes,
+	.gpl_only       = false,
+	.ret_type       = RET_INTEGER,
+	.arg1_type	= ARG_PTR_TO_CTX,
+	.arg2_type      = ARG_ANYTHING,
+};
+
 BPF_CALL_1(bpf_get_cgroup_classid, const struct sk_buff *, skb)
 {
 	return task_get_classid(skb);
