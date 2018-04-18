2ff53b167c29484b01e0356f01ad4535eb28bc11
Martin KaFai Lau <kafai@fb.com>
2018-04-18 15:55:57 -0700

    bpf: btf: Introduce BPF Type Format (BTF)
    This patch introduces BPF type Format (BTF).
    
    BTF (BPF Type Format) is the meta data format which describes
    the data types of BPF program/map.  Hence, it basically focus
    on the C programming language which the modern BPF is primary
    using.  The first use case is to provide a generic pretty print
    capability for a BPF map.
    
    BTF has its root from CTF (Compact C-Type format).  To simplify
    the handling of BTF data, BTF removes the differences between
    small and big type/struct-member.  Hence, BTF consistently uses u32
    instead of supporting both "one u16" and "two u32 (+padding)" in
    describing type and struct-member.
    
    It also raises the number of types (and functions) limit
    from 0x7fff to 0x7fffffff.
    
    Due to the above changes,  the format is not compatible to CTF.
    Hence, BTF starts with a new BTF_MAGIC and version number.
    
    This patch does the first verification pass to the BTF.  The first
    pass checks:
    1. meta-data size (e.g. It does not go beyond the total btf's size)
    2. name_offset is valid
    3. Each BTF_KIND (e.g. int, enum, struct....) does its
       own check of its meta-data.
    
    Some other checks, like checking a struct's member is referring
    to a valid type, can only be done in the second pass.  The second
    verification pass will be implemented in the next patch.
    
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Acked-by: Alexei Starovoitov <ast@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    (cherry picked from commit 69b693f0aefa0ed521e8bd02260523b5ae446ad7)
    
    Conflicts:
            kernel/bpf/Makefile: Added btf.o; dropped 4.18-only objects
              (tnum.o, bpf_lru_list.o, map_in_map.o, disasm.o, devmap.o,
              cpumap.o, offload.o, sockmap.o) not present in 4.4 tree.
            kernel/bpf/btf.c: Adapted bpf_verifier_log infrastructure to
              4.4-compatible equivalent. Upstream uses struct bpf_verifier_log
              (added in 4.18) with bpf_verifier_vlog(), bpf_verifier_log_needed(),
              bpf_verifier_log_full(). Replaced with struct btf_log embedded in
              btf_verifier_env with equivalent vscnprintf+copy_to_user
              implementation. kvmalloc() replaced with vmalloc() (added in 4.12).
            include/linux/btf.h: Added as new file with minimal forward
              declarations (struct btf, struct btf_type, union bpf_attr).
              Full function declarations (btf_put, btf_new_fd, btf_get_by_fd,
              btf_type_id_size, btf_type_seq_show) added in subsequent commits.

---
+++ include/linux/btf.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (c) 2018 Facebook */
+
+#ifndef _LINUX_BTF_H
+#define _LINUX_BTF_H 1
+
+#include <linux/types.h>
+
+struct btf;
+struct btf_type;
+union bpf_attr;
+
+#endif
