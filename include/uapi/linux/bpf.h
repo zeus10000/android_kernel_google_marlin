5cfdfc940fb8b258eb4f37f80494ad575e10f70f
Martin KaFai Lau <kafai@fb.com>
2018-04-18 15:56:01 -0700

    bpf: btf: Add BPF_BTF_LOAD command
    This patch adds a BPF_BTF_LOAD command which
    1) loads and verifies the BTF (implemented in earlier patches)
    2) returns a BTF fd to userspace.  In the next patch, the
       BTF fd can be specified during BPF_MAP_CREATE.
    
    It currently limits to CAP_SYS_ADMIN.
    
    Change-Id: I025c1894c03c38656e44cf6931fe63fafd647a32
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Acked-by: Alexei Starovoitov <ast@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    (cherry picked from commit f56a653c1fd13a197076dec4461c656fd2adec73)

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -94,6 +94,7 @@ enum bpf_cmd {
 	BPF_MAP_GET_FD_BY_ID,
 	BPF_OBJ_GET_INFO_BY_FD,
 	BPF_PROG_QUERY,
+	BPF_BTF_LOAD,
 };
 
 enum bpf_map_type {
@@ -354,6 +355,19 @@ union bpf_attr {
 		__aligned_u64	prog_ids;
 		__u32		prog_cnt;
 	} query;
+
+	struct {
+		__u64 name;
+		__u32 prog_fd;
+	} raw_tracepoint;
+
+	struct { /* anonymous struct for BPF_BTF_LOAD */
+		__aligned_u64	btf;
+		__aligned_u64	btf_log_buf;
+		__u32		btf_size;
+		__u32		btf_log_size;
+		__u32		btf_log_level;
+	};
 } __attribute__((aligned(8)));
 
 /* BPF helper function descriptions:
