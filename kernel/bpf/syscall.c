3ca4e12a7545ed530ef4829e8a14245e446f13cf
Mark Rutland <mark.rutland@arm.com>
2018-05-03 17:04:59 +0100

    bpf: fix possible spectre-v1 in find_and_alloc_map()
    It's possible for userspace to control attr->map_type. Sanitize it when
    using it as an array index to prevent an out-of-bounds value being used
    under speculation.
    
    Found by smatch.
    
    Change-Id: I236ede3ae4dcfbb10bab42f20a44ae95296e95e7
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Alexei Starovoitov <ast@kernel.org>
    Cc: Dan Carpenter <dan.carpenter@oracle.com>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: netdev@vger.kernel.org
    Acked-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    (cherry picked from commit 9ef09e35e521bf0df5325cc9cffa726a8f5f3c1b)

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -100,12 +100,14 @@ const struct bpf_map_ops bpf_map_offload_ops = {
 static struct bpf_map *find_and_alloc_map(union bpf_attr *attr)
 {
 	const struct bpf_map_ops *ops;
+	u32 type = attr->map_type;
 	struct bpf_map *map;
 	int err;
 
-	if (attr->map_type >= ARRAY_SIZE(bpf_map_types))
+	if (type >= ARRAY_SIZE(bpf_map_types))
 		return ERR_PTR(-EINVAL);
-	ops = bpf_map_types[attr->map_type];
+	type = array_index_nospec(type, ARRAY_SIZE(bpf_map_types));
+	ops = bpf_map_types[type];
 	if (!ops)
 		return ERR_PTR(-EINVAL);
 
@@ -120,7 +122,7 @@ static struct bpf_map *find_and_alloc_map(union bpf_attr *attr)
 	if (IS_ERR(map))
 		return map;
 	map->ops = ops;
-	map->map_type = attr->map_type;
+	map->map_type = type;
 	return map;
 }
 
