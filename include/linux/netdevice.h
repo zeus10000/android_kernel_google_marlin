39010ef94125b94f42d7ffa83b55c1d07cfcc552
Stephen Hemminger <stephen@networkplumber.org>
2018-07-27 13:43:23 -0700

    net: report invalid mtu value via netlink extack
    If an invalid MTU value is set through rtnetlink return extra error
    information instead of putting message in kernel log. For other cases
    where there is no visible API, keep the error report in the log.
    
    Example:
    	# ip li set dev enp12s0 mtu 10000
    	Error: mtu greater than device maximum.
    
    	# ifconfig enp12s0 mtu 10000
    	SIOCSIFMTU: Invalid argument
    	# dmesg | tail -1
    	[ 2047.795467] enp12s0: mtu greater than device maximum
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 7a4c53bee3324ac00bf964aa2f82d15d279e86e4)

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -3372,6 +3372,8 @@ int dev_change_name(struct net_device *, const char *);
 int dev_set_alias(struct net_device *, const char *, size_t);
 int dev_change_net_namespace(struct net_device *, struct net *, const char *);
 int __dev_set_mtu(struct net_device *, int);
+int dev_set_mtu_ext(struct net_device *dev, int mtu,
+		    struct netlink_ext_ack *extack);
 int dev_set_mtu(struct net_device *, int);
 int dev_change_tx_queue_len(struct net_device *, unsigned long);
 void dev_set_group(struct net_device *, int);
