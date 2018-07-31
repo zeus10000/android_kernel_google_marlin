53851729f8ec530fe64ce7c9b80ff05014698b89
Arthur Fabre <afabre@cloudflare.com>
2018-07-31 18:17:22 +0100

    bpf: verifier: MOV64 don't mark dst reg unbounded
    When check_alu_op() handles a BPF_MOV64 between two registers,
    it calls check_reg_arg(DST_OP) on the dst register, marking it
    as unbounded. If the src and dst register are the same, this
    marks the src as unbounded, which can lead to unexpected errors
    for further checks that rely on bounds info. For example:
    
    	BPF_MOV64_IMM(BPF_REG_2, 0),
    	BPF_MOV64_REG(BPF_REG_2, BPF_REG_2),
    	BPF_ALU64_REG(BPF_ADD, BPF_REG_1, BPF_REG_2),
    	BPF_MOV64_IMM(BPF_REG_0, 0),
    	BPF_EXIT_INSN(),
    
    Results in:
    
    	"math between ctx pointer and register with unbounded
    	min value is not allowed"
    
    check_alu_op() now uses check_reg_arg(DST_OP_NO_MARK), and MOVs
    that need to mark the dst register (MOVIMM, MOV32) do so.
    
    Added a test case for MOV64 dst == src, and dst != src.
    
    Change-Id: I0aefa6c3b944504a498e7a62e63d51e5a3c78e74
    Signed-off-by: Arthur Fabre <afabre@cloudflare.com>
    Acked-by: Edward Cree <ecree@solarflare.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    (cherry picked from commit fbeb1603bf4e9baa82da8f794de42949d0fe5e25)

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -3484,8 +3484,8 @@ static int check_alu_op(struct bpf_verifier_env *env, struct bpf_insn *insn)
 			}
 		}
 
-		/* check dest operand */
-		err = check_reg_arg(env, insn->dst_reg, DST_OP);
+		/* check dest operand, mark as required later */
+		err = check_reg_arg(env, insn->dst_reg, DST_OP_NO_MARK);
 		if (err)
 			return err;
 
