42588f1bc205f0c34fefacb765614ca0429cf613
Daniel Borkmann <daniel@iogearbox.net>
2018-10-21 02:09:24 +0200

    bpf, verifier: fix register type dump in xadd and st
    Using reg_type_str[insn->dst_reg] is incorrect since insn->dst_reg
    contains the register number but not the actual register type. Add
    a small reg_state() helper and use it to get to the type. Also fix
    up the test_verifier test cases that have an incorrect errstr.
    
    Fixes: 9d2be44a7f33 ("bpf: Reuse canonical string formatter for ctx errs")
    Change-Id: I7d220e5f79f7c7960a011bdf2fe99202f009d404
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    (cherry picked from commit 2a159c6f82381a458bc56e7e202b6bee57a2ccb7)

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -1540,9 +1540,14 @@ static bool __is_pointer_value(bool allow_ptr_leaks,
 	return reg->type != SCALAR_VALUE;
 }
 
+static struct bpf_reg_state *reg_state(struct bpf_verifier_env *env, int regno)
+{
+	return cur_regs(env) + regno;
+}
+
 static bool is_pointer_value(struct bpf_verifier_env *env, int regno)
 {
-	return __is_pointer_value(env->allow_ptr_leaks, cur_regs(env) + regno);
+	return __is_pointer_value(env->allow_ptr_leaks, reg_state(env, regno));
 }
 
 static bool is_ctx_reg(struct bpf_verifier_env *env, int regno)
@@ -1947,7 +1952,7 @@ static int check_stack_boundary(struct bpf_verifier_env *env, int regno,
 				int access_size, bool zero_size_allowed,
 				struct bpf_call_arg_meta *meta)
 {
-	struct bpf_reg_state *reg = cur_regs(env) + regno;
+	struct bpf_reg_state *reg = reg_state(env, regno);
 	struct bpf_func_state *state = func(env, reg);
 	int off, i, slot, spi;
 
