314c07e634376519a8b1292a4c696ccc01529014
Daniel Borkmann <daniel@iogearbox.net>
2018-10-24 22:05:49 +0200

    bpf: make direct packet write unclone more robust
    Given this seems to be quite fragile and can easily slip through the
    cracks, lets make direct packet write more robust by requiring that
    future program types which allow for such write must provide a prologue
    callback. In case of XDP and sk_msg it's noop, thus add a generic noop
    handler there. The latter starts out with NULL data/data_end unconditionally
    when sg pages are shared.
    
    Change-Id: I2decc76d61fed2b7114e1a9d447bcdee54ba7456
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Song Liu <songliubraving@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    (cherry picked from commit b09928b976280d64060d7bee146d7df5c5a29bef)

--- net/core/filter.c
+++ net/core/filter.c
@@ -5408,6 +5408,15 @@ static bool sock_filter_is_valid_access(int off, int size,
 					       prog->expected_attach_type);
 }
 
+static int bpf_noop_prologue(struct bpf_insn *insn_buf, bool direct_write,
+			     const struct bpf_prog *prog)
+{
+	/* Neither direct read nor direct write requires any preliminary
+	 * action.
+	 */
+	return 0;
+}
+
 static int bpf_unclone_prologue(struct bpf_insn *insn_buf, bool direct_write,
 				const struct bpf_prog *prog, int drop_verdict)
 {
@@ -6821,6 +6830,7 @@ const struct bpf_verifier_ops xdp_verifier_ops = {
 	.get_func_proto		= xdp_func_proto,
 	.is_valid_access	= xdp_is_valid_access,
 	.convert_ctx_access	= xdp_convert_ctx_access,
+	.gen_prologue		= bpf_noop_prologue,
 };
 
 const struct bpf_prog_ops xdp_prog_ops = {
