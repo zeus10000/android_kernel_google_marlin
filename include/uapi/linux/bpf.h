12e3d60d8f4113b6d6c9f43ba5ad79828ed6f690
Lorenz Bauer <lmb@cloudflare.com>
2018-11-16 11:41:09 +0000

    bpf: move BPF_F_QUERY_EFFECTIVE after map flags
    BPF_F_QUERY_EFFECTIVE is in the middle of the flags valid
    for BPF_MAP_CREATE. Move it to its own section to reduce confusion.
    
    Change-Id: Idfe3e4da024a6189233bbc35e07f4a62b5b5f6f0
    Signed-off-by: Lorenz Bauer <lmb@cloudflare.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    (cherry picked from commit 2f1833607aed6a9c1e1729bf0e2588c341ceb409)

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -257,6 +257,9 @@ enum bpf_attach_type {
 /* Zero-initialize hash function seed. This should only be used for testing. */
 #define BPF_F_ZERO_SEED		(1U << 6)
 
+/* flags for BPF_PROG_QUERY */
+#define BPF_F_QUERY_EFFECTIVE	(1U << 0)
+
 enum bpf_stack_build_id_status {
 	/* user space need an empty entry to identify end of a trace */
 	BPF_STACK_BUILD_ID_EMPTY = 0,
