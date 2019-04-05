a254ae1b0838ef7c960b0518d26d872efe031198
David Ahern <dsahern@gmail.com>
2019-04-05 16:30:25 -0700

    ipv6: Add neighbor helpers that use the ipv6 stub
    Add ipv6 helpers to handle ndisc references via the stub. Update
    bpf_ipv6_fib_lookup to use __ipv6_neigh_lookup_noref_stub instead of
    the open code ___neigh_lookup_noref with the stub.
    
    Change-Id: Ibd94a61da699413d3de31059057932e7ce65ee50
    Signed-off-by: David Ahern <dsahern@gmail.com>
    Reviewed-by: Ido Schimmel <idosch@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 71df5777aaaeff673c242a49b945b1b96fe81718)

--- include/net/ndisc.h
+++ include/net/ndisc.h
@@ -2,6 +2,8 @@
 #ifndef _NDISC_H
 #define _NDISC_H
 
+#include <net/ipv6_stubs.h>
+
 /*
  *	ICMP codes for neighbour discovery messages
  */
@@ -159,6 +161,14 @@ static inline u32 ndisc_hashfn(const void *pkey, const struct net_device *dev, _
 static inline struct neighbour *__ipv6_neigh_lookup_noref(struct net_device *dev, const void *pkey)
 {
 	return ___neigh_lookup_noref(&nd_tbl, neigh_key_eq128, ndisc_hashfn, pkey, dev);
+}
+
+static inline
+struct neighbour *__ipv6_neigh_lookup_noref_stub(struct net_device *dev,
+						 const void *pkey)
+{
+	return ___neigh_lookup_noref(ipv6_stub->nd_tbl, neigh_key_eq128,
+				     ndisc_hashfn, pkey, dev);
 }
 
 static inline struct neighbour *__ipv6_neigh_lookup(struct net_device *dev, const void *pkey)
