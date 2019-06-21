eea781e813259c522a69d7a0d792af5fc61e84fb
Jason A. Donenfeld <Jason@zx2c4.com>
2019-06-21 22:32:48 +0200

    timekeeping: Use proper clock specifier names in functions
    This makes boot uniformly boottime and tai uniformly clocktai, to
    address the remaining oversights.
    
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>
    Link: https://lkml.kernel.org/r/20190621203249.3909-2-Jason@zx2c4.com
    
    (cherry picked from commit 9285ec4c8b61d4930a575081abeba2cd4f449a74)
    Change-Id: Icf0cd8d6116d91ec54691ed79b4daf834b4dc009

--- kernel/fork.c
+++ kernel/fork.c
@@ -1722,7 +1722,7 @@ static struct task_struct *copy_process(unsigned long clone_flags,
 	 */
 
 	p->start_time = ktime_get_ns();
-	p->real_start_time = ktime_get_boot_ns();
+	p->real_start_time = ktime_get_boottime_ns();
 
 	/*
 	 * Make it visible to the rest of the system, but dont wake it up yet.
