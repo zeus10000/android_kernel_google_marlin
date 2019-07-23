04ad9d127cbb154b8a6fdd7a90217f811886ad36
Matthew Wilcox (Oracle) <willy@infradead.org>
2019-07-22 20:08:26 -0700

    net: Use skb accessors in network core
    In preparation for unifying the skb_frag and bio_vec, use the fine
    accessors which already exist and use skb_frag_t instead of
    struct skb_frag_struct.
    
    Change-Id: Ie78eb1f40e36820da8d6cb27d243afa2b186a210
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit d8e18a516f8f67404c0d21af8c93d0474fba0876)

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -2785,7 +2785,7 @@ static inline bool skb_can_coalesce(struct sk_buff *skb, int i,
 				    const struct page *page, int off)
 {
 	if (i) {
-		const struct skb_frag_struct *frag = &skb_shinfo(skb)->frags[i - 1];
+		const skb_frag_t *frag = &skb_shinfo(skb)->frags[i - 1];
 
 		return page == skb_frag_page(frag) &&
 		       off == frag->page_offset + skb_frag_size(frag);
