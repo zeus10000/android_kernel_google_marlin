fcc091dd413f911c52db65e3a693fb8f55962a4c
Alexei Starovoitov <ast@kernel.org>
2019-11-14 10:57:16 -0800

    BACKPORT: bpf: Compare BTF types of functions arguments with actual types
    Make the verifier check that BTF types of function arguments match actual types
    passed into top-level BPF program and into BPF-to-BPF calls. If types match
    such BPF programs and sub-programs will have full support of BPF trampoline. If
    types mismatch the trampoline has to be conservative. It has to save/restore
    five program arguments and assume 64-bit scalars.
    
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Song Liu <songliubraving@fb.com>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Link: https://lore.kernel.org/bpf/20191114185720.1641606-17-ast@kernel.org
    
    [auto-resolved: BPF-core=INCOMING, non-BPF=OURS]
    (cherry picked from commit 8c1b6e69dcc1e11bd24111e3734dd740aaf3fda1)
    Change-Id: Ib8ce4169d1d92cfea663f8bcbe935617ef1e8707

--- include/linux/bpf_verifier.h
+++ include/linux/bpf_verifier.h
@@ -355,6 +355,7 @@ struct bpf_ext_analyzer_ops {
 #define BPF_MAX_SUBPROGS 256
 
 struct bpf_subprog_info {
+	/* 'start' has to be the first field otherwise find_subprog() won't work */
 	u32 start; /* insn idx of function entry point */
 	u32 linfo_idx; /* The idx to the main_prog->aux->linfo */
 	u16 stack_depth; /* max. stack depth used by this function */
