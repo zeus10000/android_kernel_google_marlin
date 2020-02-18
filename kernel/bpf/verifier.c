fc59c55fe2b3d183f5ef15c88bd5ef2a1348458b
Jakub Sitnicki <jakub@cloudflare.com>
2020-02-18 17:10:20 +0000

    UPSTREAM: bpf: Allow selecting reuseport socket from a SOCKMAP/SOCKHASH
    SOCKMAP & SOCKHASH now support storing references to listening
    sockets. Nothing keeps us from using these map types a collection of
    sockets to select from in BPF reuseport programs. Whitelist the map types
    with the bpf_sk_select_reuseport helper.
    
    The restriction that the socket has to be a member of a reuseport group
    still applies. Sockets in SOCKMAP/SOCKHASH that don't have sk_reuseport_cb
    set are not a valid target and we signal it with -EINVAL.
    
    The main benefit from this change is that, in contrast to
    REUSEPORT_SOCKARRAY, SOCK{MAP,HASH} don't impose a restriction that a
    listening socket can be just one BPF map at the same time.
    
    Change-Id: Id01c908aeb49f7b3acd0e76691353b107788d9d4
    Signed-off-by: Jakub Sitnicki <jakub@cloudflare.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Link: https://lore.kernel.org/bpf/20200218171023.844439-9-jakub@cloudflare.com
    (cherry picked from commit 9fed9000c5c6cacfcaaa48aff74818072ae294cc)

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -3694,14 +3694,16 @@ static int check_map_func_compatibility(struct bpf_verifier_env *env,
 		if (func_id != BPF_FUNC_sk_redirect_map &&
 		    func_id != BPF_FUNC_sock_map_update &&
 		    func_id != BPF_FUNC_map_delete_elem &&
-		    func_id != BPF_FUNC_msg_redirect_map)
+		    func_id != BPF_FUNC_msg_redirect_map &&
+		    func_id != BPF_FUNC_sk_select_reuseport)
 			goto error;
 		break;
 	case BPF_MAP_TYPE_SOCKHASH:
 		if (func_id != BPF_FUNC_sk_redirect_hash &&
 		    func_id != BPF_FUNC_sock_hash_update &&
 		    func_id != BPF_FUNC_map_delete_elem &&
-		    func_id != BPF_FUNC_msg_redirect_hash)
+		    func_id != BPF_FUNC_msg_redirect_hash &&
+		    func_id != BPF_FUNC_sk_select_reuseport)
 			goto error;
 		break;
 	case BPF_MAP_TYPE_REUSEPORT_SOCKARRAY:
@@ -3775,7 +3777,9 @@ static int check_map_func_compatibility(struct bpf_verifier_env *env,
 			goto error;
 		break;
 	case BPF_FUNC_sk_select_reuseport:
-		if (map->map_type != BPF_MAP_TYPE_REUSEPORT_SOCKARRAY)
+		if (map->map_type != BPF_MAP_TYPE_REUSEPORT_SOCKARRAY &&
+		    map->map_type != BPF_MAP_TYPE_SOCKMAP &&
+		    map->map_type != BPF_MAP_TYPE_SOCKHASH)
 			goto error;
 		break;
 	case BPF_FUNC_map_peek_elem:
