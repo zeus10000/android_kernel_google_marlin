7b7ae128a726377ad5428872c9a23677fab9332e
Eric Dumazet <edumazet@google.com>
2020-04-22 09:13:28 -0700

    net: napi: use READ_ONCE()/WRITE_ONCE()
    gro_flush_timeout and napi_defer_hard_irqs can be read
    from napi_complete_done() while other cpus write the value,
    whithout explicit synchronization.
    
    Use READ_ONCE()/WRITE_ONCE() to annotate the races.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 7e417a66b86c110f4b282945dac82e21e0b08328)

--- net/core/net-sysfs.c
+++ net/core/net-sysfs.c
@@ -332,7 +332,7 @@ NETDEVICE_SHOW_RW(tx_queue_len, fmt_ulong);
 
 static int change_gro_flush_timeout(struct net_device *dev, unsigned long val)
 {
-	dev->gro_flush_timeout = val;
+	WRITE_ONCE(dev->gro_flush_timeout, val);
 	return 0;
 }
 
@@ -349,7 +349,7 @@ NETDEVICE_SHOW_RW(gro_flush_timeout, fmt_ulong);
 
 static int change_napi_defer_hard_irqs(struct net_device *dev, unsigned long val)
 {
-	dev->napi_defer_hard_irqs = val;
+	WRITE_ONCE(dev->napi_defer_hard_irqs, val);
 	return 0;
 }
 
