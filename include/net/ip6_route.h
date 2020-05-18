e1ec8001537c4751752991a2132b0f9c87d420d9
Christoph Hellwig <hch@lst.de>
2020-05-18 08:28:05 +0200

    BACKPORT: ipv6: lift copy_from_user out of ipv6_route_ioctl
    Prepare for better compat ioctl handling by moving the user copy out
    of ipv6_route_ioctl.
    
    Change-Id: I908f9b2f7d7e0b641817397db7b527f4004e7593
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 7c1552da900c159a13473a2106c7547746ebe4a1)

--- include/net/ip6_route.h
+++ include/net/ip6_route.h
@@ -83,7 +83,8 @@ void ip6_route_init_special_entries(void);
 int ip6_route_init(void);
 void ip6_route_cleanup(void);
 
-int ipv6_route_ioctl(struct net *net, unsigned int cmd, void __user *arg);
+int ipv6_route_ioctl(struct net *net, unsigned int cmd,
+		struct in6_rtmsg *rtmsg);
 
 int ip6_route_add(struct fib6_config *cfg);
 int ip6_ins_rt(struct rt6_info *);
