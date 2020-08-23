b2fd57a32d8153956df5268539925591fdc3f780
Gustavo A. R. Silva <gustavoars@kernel.org>
2020-08-23 17:36:59 -0500

    BACKPORT: treewide: Use fallthrough pseudo-keyword
    Replace the existing /* fall through */ comments and its variants with
    the new pseudo-keyword macro fallthrough[1]. Also, remove unnecessary
    fall-through markings when it is the case.
    
    [1] https://www.kernel.org/doc/html/v5.7/process/deprecated.html?highlight=fallthrough#implicit-switch-case-fall-through
    
    Change-Id: I5457d15f4f51b6b5012e1ec9b65a06cd9cde7fad
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    (cherry picked from commit df561f6688fef775baa341a0f5d960becd248b11)

--- kernel/kallsyms.c
+++ kernel/kallsyms.c
@@ -621,11 +621,11 @@ bool kallsyms_show_value(const struct cred *cred)
 	case 0:
 		if (kallsyms_for_perf())
 			return true;
-	/* fallthrough */
+		fallthrough;
 	case 1:
 		if (security_capable(cred, &init_user_ns, CAP_SYSLOG) == 0)
 			return true;
-	/* fallthrough */
+		fallthrough;
 	default:
 		return false;
 	}
