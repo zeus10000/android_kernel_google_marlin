f535095fb8a4881c2924b770aa294472d2f736f5
Gustavo A. R. Silva <gustavoars@kernel.org>
2020-08-23 17:36:59 -0500

    BACKPORT: treewide: Use fallthrough pseudo-keyword
    Replace the existing /* fall through */ comments and its variants with
    the new pseudo-keyword macro fallthrough[1]. Also, remove unnecessary
    fall-through markings when it is the case.
    
    [1] https://www.kernel.org/doc/html/v5.7/process/deprecated.html?highlight=fallthrough#implicit-switch-case-fall-through
    
    Change-Id: I9e52747202fb56545c2319d6faa8a8a222ec7b2f
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    (cherry picked from commit df561f6688fef775baa341a0f5d960becd248b11)

--- include/linux/filter.h
+++ include/linux/filter.h
@@ -1183,7 +1183,7 @@ static inline u16 bpf_anc_helper(const struct sock_filter *ftest)
 		BPF_ANCILLARY(RANDOM);
 		BPF_ANCILLARY(VLAN_TPID);
 		}
-		/* Fallthrough. */
+		fallthrough;
 	default:
 		return ftest->code;
 	}
