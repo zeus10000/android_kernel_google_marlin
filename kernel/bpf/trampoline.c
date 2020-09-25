7cac0d16493b5bebdccdd509ece7d300e25590d0
Toke Høiland-Jørgensen <toke@redhat.com>
2020-09-25 23:25:02 +0200

    BACKPORT: bpf: verifier: refactor check_attach_btf_id()
    The check_attach_btf_id() function really does three things:
    
    1. It performs a bunch of checks on the program to ensure that the
       attachment is valid.
    
    2. It stores a bunch of state about the attachment being requested in
       the verifier environment and struct bpf_prog objects.
    
    3. It allocates a trampoline for the attachment.
    
    This patch splits out (1.) and (3.) into separate functions which will
    perform the checks, but return the computed values instead of directly
    modifying the environment. This is done in preparation for reusing the
    checks when the actual attachment is happening, which will allow tracing
    programs to have multiple (compatible) attachments.
    
    This also fixes a bug where a bunch of checks were skipped if a trampoline
    already existed for the tracing target.
    
    Fixes: 6ba43b761c41 ("bpf: Attachment verification for BPF_MODIFY_RETURN")
    Fixes: 1e6c62a88215 ("bpf: Introduce sleepable BPF programs")
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Change-Id: I2b62aa0f819d87c6214bdec385d9b8b703a577dc
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    (cherry picked from commit f7b12b6fea00988496b7606d4964cd77beef46a5)

--- kernel/bpf/trampoline.c
+++ kernel/bpf/trampoline.c
@@ -4,7 +4,7 @@
 #include <linux/bpf.h>
 #include <linux/filter.h>
 
-struct bpf_trampoline *bpf_trampoline_lookup(u64 key)
+static struct bpf_trampoline *bpf_trampoline_lookup(u64 key)
 {
 	return NULL;
 }
@@ -17,6 +17,26 @@ int bpf_trampoline_link_prog(struct bpf_prog *prog)
 int bpf_trampoline_unlink_prog(struct bpf_prog *prog)
 {
 	return -EOPNOTSUPP;
+}
+
+struct bpf_trampoline *bpf_trampoline_get(u64 key,
+					  struct bpf_attach_target_info *tgt_info)
+{
+	struct bpf_trampoline *tr;
+
+	tr = bpf_trampoline_lookup(key);
+	if (!tr)
+		return NULL;
+
+	mutex_lock(&tr->mutex);
+	if (tr->func.addr)
+		goto out;
+
+	memcpy(&tr->func.model, &tgt_info->fmodel, sizeof(tgt_info->fmodel));
+	tr->func.addr = (void *)tgt_info->tgt_addr;
+out:
+	mutex_unlock(&tr->mutex);
+	return tr;
 }
 
 void bpf_trampoline_put(struct bpf_trampoline *tr)
