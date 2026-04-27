98fd900f5ad539edaa2dcb7483573fd4ba0a3f73
Builder <builder@localhost>
2026-04-27 00:15:12 +0000

    Revert BPF cherry-pick side effects in drivers/char/mem.c
    Reverts access_ok() 2-arg form back to 3-arg VERIFY_*. Reference:
    96d8a72c7c0e (uaccess: removal of unused arg in access_ok). Upstream
    remains applied in files where its prerequisites are satisfied.
    
    Targeted revert; affects drivers/char/mem.c only.
    
    Change-Id: Id97950cda72555290a77fc796ac3306bb65ff0db

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: GPL-2.0
 /*
  *  linux/drivers/char/mem.c
  *
@@ -604,7 +603,7 @@ static ssize_t read_port(struct file *file, char __user *buf,
 	unsigned long i = *ppos;
 	char __user *tmp = buf;
 
-	if (!access_ok(buf, count))
+	if (!access_ok(VERIFY_WRITE, buf, count))
 		return -EFAULT;
 	while (count-- > 0 && i < 65536) {
 		if (__put_user(inb(i), tmp) < 0)
@@ -622,7 +621,7 @@ static ssize_t write_port(struct file *file, const char __user *buf,
 	unsigned long i = *ppos;
 	const char __user *tmp = buf;
 
-	if (!access_ok(buf, count))
+	if (!access_ok(VERIFY_READ, buf, count))
 		return -EFAULT;
 	while (count-- > 0 && i < 65536) {
 		char c;
