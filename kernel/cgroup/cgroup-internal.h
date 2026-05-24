9f59f98c5b45f806551530f7e0c9d25552ed17b6
Builder <builder@localhost>
2026-05-24 07:03:59 +0000

    BACKPORT: bpf: layer-25 — create kernel/cgroup/cgroup-internal.h stub
    Change-Id: I679a0dd1a3f2954240f0c9b91ed30531d714a91a

---
+++ kernel/cgroup/cgroup-internal.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* marlin v4.4 stub for kernel/cgroup/cgroup-internal.h */
+#ifndef __CGROUP_INTERNAL_H
+#define __CGROUP_INTERNAL_H
+
+#include <linux/cgroup.h>
+
+#endif
