affebe939ebe400957644b32c9ca64c815163842
Builder <builder@localhost>
2026-05-25 17:43:29 +0000

    marlin: tcp_v6_pre_connect dedup; udp.c 604 sdif=0 inserted
    Change-Id: Ie5f4969e2503f1a1d40c46b56551fd0c69237cad

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -602,7 +602,7 @@ static inline struct sock *__udp4_lib_lookup_skb(struct sk_buff *skb,
 	const struct iphdr *iph = ip_hdr(skb);
 
 	return __udp4_lib_lookup(dev_net(skb_dst(skb)->dev), iph->saddr, sport,
-				 iph->daddr, dport, inet_iif(skb),
+				 iph->daddr, dport, inet_iif(skb), 0,
 				 udptable, skb);
 }
 
