f69f6be626f2519a826699ae509ee2d5522e82c9
Builder <builder@localhost>
2026-05-25 16:52:07 +0000

    marlin: remove sock_get_timestamp stub (real def in sock.c)
    Change-Id: I0ba0528c831138c807961d32b851746f1c09a000

--- include/net/sock.h
+++ include/net/sock.h
@@ -2494,6 +2494,5 @@ struct saved_syn {
 	u8 data[];
 };
 
-static inline int sock_get_timestamp(struct sock *sk, struct timeval __user *tv) { return -EINVAL; }
 #endif	/* _SOCK_H */
 #define sock_owned_by_me(sk) sock_owned_by_user(sk)
