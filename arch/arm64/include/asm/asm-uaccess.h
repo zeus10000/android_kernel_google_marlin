35ba17fe0865cd50dcc17617fcef9f2abdff710a
Builder <builder@localhost>
2026-05-25 05:05:50 +0000

    marlin: asm-uaccess.h remove dup macros (already in uaccess.h)
    Change-Id: Iea73aec9695588eba222206fd49b90cff479c42c

--- arch/arm64/include/asm/asm-uaccess.h
+++ arch/arm64/include/asm/asm-uaccess.h
@@ -12,34 +12,6 @@
 	.endm
 
 
-/* marlin: ASM uaccess_ttbr0_enable/disable macros */
-	.macro	uaccess_ttbr0_disable, tmp1, tmp2
-alternative_if_not ARM64_HAS_PAN
-	save_and_disable_irq \tmp2
-	__uaccess_ttbr0_disable \tmp1
-	restore_irq \tmp2
-alternative_else_nop_endif
-	.endm
 
-	.macro	uaccess_ttbr0_enable, tmp1, tmp2, tmp3
-alternative_if_not ARM64_HAS_PAN
-	save_and_disable_irq \tmp3
-	__uaccess_ttbr0_enable \tmp1, \tmp2
-	restore_irq \tmp3
-alternative_else_nop_endif
-	.endm
-
-	.macro	__uaccess_ttbr0_disable, tmp1
-	mrs	\tmp1, ttbr1_el1
-	mov	\tmp1, #0
-	msr	ttbr0_el1, \tmp1
-	isb
-	.endm
-
-	.macro	__uaccess_ttbr0_enable, tmp1, tmp2
-	mrs	\tmp1, ttbr1_el1
-	msr	ttbr0_el1, \tmp1
-	isb
-	.endm
 
 #endif
