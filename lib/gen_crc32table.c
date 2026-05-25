c16e7b9ace009748e65cefdf02343994cc358e2f
Builder <builder@localhost>
2026-05-25 18:46:20 +0000

    marlin: gen_crc32table inline CRC defines
    Change-Id: Ib2946c15e31630fcbaec53614eb206147f43291c

--- lib/gen_crc32table.c
+++ lib/gen_crc32table.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <stdio.h>
-/* marlin: autoconf removed */
+#define CONFIG_CRC32_SLICEBY8 1
+#define CONFIG_CRC32_BIT 1
 #include "crc32defs.h"
 #include <inttypes.h>
 
