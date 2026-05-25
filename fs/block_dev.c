47b9c6ce5602bdc9666d43613b0a8beb633547f0
Builder <builder@localhost>
2026-05-25 04:36:42 +0000

    marlin: block_dev.c drop __pmem annotation
    Change-Id: I6853aea9a4d3442b45994a9412a313d971a41624

--- fs/block_dev.c
+++ fs/block_dev.c
@@ -465,7 +465,7 @@ EXPORT_SYMBOL_GPL(bdev_write_page);
  * accessible at this address.
  */
 long bdev_direct_access(struct block_device *bdev, sector_t sector,
-			void __pmem **addr, unsigned long *pfn, long size)
+			void **addr, unsigned long *pfn, long size)
 {
 	long avail;
 	const struct block_device_operations *ops = bdev->bd_disk->fops;
