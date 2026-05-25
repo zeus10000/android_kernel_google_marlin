4eed49d83df5094d5709cb64f40ad3f73da0fc05
Builder <builder@localhost>
2026-05-25 19:01:24 +0000

    marlin: more weak stubs - udpv6_offload_exit ipv6_anycast_cleanup tcp_sendmsg_locked ipv6_list_rcv __cgroup_bpf_run_filter_skb
    Change-Id: I4967f317a9a22d152f38807b7262c64dcfad653f

--- kernel/bpf/marlin_stubs.c
+++ kernel/bpf/marlin_stubs.c
@@ -26,3 +26,14 @@ int sock_gettstamp(struct socket *sock, void __user *userstamp, bool timeval, bo
 	return -ENOIOCTLCMD;
 }
 EXPORT_SYMBOL(sock_gettstamp);
+
+#include <net/sock.h>
+#include <linux/skbuff.h>
+#include <linux/bpf-cgroup.h>
+
+void udpv6_offload_exit(void) { }
+void ipv6_anycast_cleanup(void) { }
+int tcp_sendmsg_locked(struct sock *sk, struct msghdr *msg, size_t size) { return -EINVAL; }
+int tcp_sendpage_locked(struct sock *sk, struct page *page, int offset, size_t size, int flags) { return -EINVAL; }
+void ipv6_list_rcv(struct list_head *head, struct packet_type *pt, struct net_device *orig_dev) { }
+int __cgroup_bpf_run_filter_skb(struct sock *sk, struct sk_buff *skb, enum bpf_attach_type type) { return 0; }
