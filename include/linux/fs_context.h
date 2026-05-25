da0d771a8c36594ee57d3134d764c010a39c0023
Builder <builder@localhost>
2026-05-25 04:11:56 +0000

    marlin: add MAX_BPF_CGROUP_STORAGE_TYPE, stub fs_context.h, fs/proc sched/task.h includes
    Change-Id: I1e4832622225fe36526b72d136493190819fe86c

---
+++ include/linux/fs_context.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* marlin: stub fs_context.h - v5.x file system context API not used */
+#ifndef _LINUX_FS_CONTEXT_H
+#define _LINUX_FS_CONTEXT_H
+struct fs_context;
+struct fs_parameter;
+#endif
