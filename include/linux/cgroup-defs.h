d90b0e5bc9f51e05f2f867a68361b2dcfc25d078
Builder <builder@localhost>
2026-05-25 04:52:06 +0000

    marlin: cgroup-defs.h cgroup_bpf as pointer
    Change-Id: Iedd88e59ab3d55b52d50f97ab76f3f369cde0e03

--- include/linux/cgroup-defs.h
+++ include/linux/cgroup-defs.h
@@ -304,7 +304,7 @@ struct cgroup {
 	struct work_struct release_agent_work;
 
 	/* used to store eBPF programs */
-	struct cgroup_bpf bpf;
+	struct cgroup_bpf *bpf; /* marlin: pointer */
 
 	/* ids of the ancestors at each level including self */
 	int ancestor_ids[];
