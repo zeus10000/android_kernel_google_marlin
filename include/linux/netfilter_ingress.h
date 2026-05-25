7d2a76712119524ddde3f0fe40cdbe4cf3a12704
Builder <builder@localhost>
2026-05-25 14:42:48 +0000

    marlin: replace netfilter_ingress.h with clean stub
    Change-Id: Ia186aa0f6df669c46241faf2bca138880845e7a9

--- include/linux/netfilter_ingress.h
+++ include/linux/netfilter_ingress.h
@@ -1,4 +1,3 @@
-/* SPDX-License-Identifier: GPL-2.0 */
 #ifndef _NETFILTER_INGRESS_H_
 #define _NETFILTER_INGRESS_H_
 
@@ -8,45 +7,16 @@
 #ifdef CONFIG_NETFILTER_INGRESS
 static inline bool nf_hook_ingress_active(const struct sk_buff *skb)
 {
-#ifdef CONFIG_JUMP_LABEL
-	if (!static_key_false(&nf_hooks_needed[NFPROTO_NETDEV][NF_NETDEV_INGRESS]))
-		return false;
-#endif
-	return rcu_access_pointer(skb->dev->nf_hooks_ingress);
+	return false;
 }
 
-/* caller must hold rcu_read_lock */
 static inline int nf_hook_ingress(struct sk_buff *skb)
 {
-	return 0; /* marlin: ingress hook stubbed v5.x sig drift */
-#if 0
-	struct nf_hook_entries *e = rcu_dereference(skb->dev->nf_hooks_ingress);
-	struct nf_hook_state state;
-	int ret;
-
-	/* Must recheck the ingress hook head, in the event it became NULL
-	 * after the check in nf_hook_ingress_active evaluated to true.
-	 */
-	if (unlikely(!e))
-		return 0;
-
-	/* marlin: v5.x nf_hook_state_init args */
-	nf_hook_state_init(&state, e, NF_NETDEV_INGRESS,
-			   INT_MIN, NFPROTO_NETDEV, skb->dev,
-			   NULL, NULL, dev_net(skb->dev), NULL);
-	ret = nf_hook_slow(skb, &state);
-	if (ret == 0)
-		return -1;
-
-	return ret;
-#endif
+	return 0;
 }
 
-static inline void nf_hook_ingress_init(struct net_device *dev)
-{
-	RCU_INIT_POINTER(dev->nf_hooks_ingress, NULL);
-}
-#else /* CONFIG_NETFILTER_INGRESS */
+static inline void nf_hook_ingress_init(struct net_device *dev) {}
+#else
 static inline int nf_hook_ingress_active(struct sk_buff *skb)
 {
 	return 0;
@@ -54,11 +24,10 @@ static inline int nf_hook_ingress_active(struct sk_buff *skb)
 
 static inline int nf_hook_ingress(struct sk_buff *skb)
 {
-	return 0; /* marlin: ingress hook stubbed v5.x sig drift */
-#if 0
 	return 0;
 }
 
 static inline void nf_hook_ingress_init(struct net_device *dev) {}
-#endif /* CONFIG_NETFILTER_INGRESS */
-#endif /* _NETFILTER_INGRESS_H_ */
+#endif
+
+#endif
